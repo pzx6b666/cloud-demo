f7356ccf31a560fe850821b8b3452ec1df81b395
pengzx <1105796453>
2022-08-23 11:48:07 +0800

    3232

--- user-service/src/main/java/com/UserApplication.java
+++ user-service/src/main/java/com/UserApplication.java
@@ -12,7 +12,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
 @MapperScan(value = "com.dao")
 public class UserApplication {
-
+//111
     public static void main(String[] args)
     {
         SpringApplication.run(UserApplication.class, args);
