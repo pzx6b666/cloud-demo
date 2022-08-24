438266b38b8de9535faa5107c2ac51093b42d654
pengzx <1105796453>
2022-08-24 16:01:35 +0800

    www

--- user-service/src/main/java/com/controller/UserController.java
+++ user-service/src/main/java/com/controller/UserController.java
@@ -11,7 +11,7 @@ import javax.annotation.Resource;
 @RequestMapping(value ="/user")
 @RestController
 public class UserController {
-   //222221111zzzzzzzwwwwwww
+   //1
     @Resource
     private RemoteSyncService remoteSyncService;
 
